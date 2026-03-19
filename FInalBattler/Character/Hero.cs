6f8c200a312b44cb237dae4c952386ac3d8d1a17
Lance Boylan <eg01st3220@gmail.com>
2026-03-19 15:24:32 -0600

    Part 1 done

--- FInalBattler/Character/Hero.cs
+++ FInalBattler/Character/Hero.cs
@@ -18,6 +18,66 @@ namespace FinalBattler.Character
 
         public Hero()
         {
+            Name = "Unknown";
+            Level = 0;
+            Health = 10;
+            Power = 1;
+            Luck = 1;
+            Mana = 1;
+        }
+
+        public void DisplayStats(bool showTotalStats = false)
+        {
+            if (showTotalStats)
+            {
+                CalculateTotals();
+                Console.WriteLine(Level);
+                Console.WriteLine(TotalHealth);
+                Console.WriteLine(TotalPower);
+                Console.WriteLine(TotalLuck);
+                Console.WriteLine(Mana);
+            }
+            else
+            {
+                Console.WriteLine(Level);
+                Console.WriteLine(Health);
+                Console.WriteLine(Power);
+                Console.WriteLine(Luck);
+                Console.WriteLine(Mana);
+            }
+        }
+        public void LevelUp()
+        {
+            Random random = new Random();
+            Level++;
+            switch (CombatClass)
+            {
+                case CombatClass.Wizard: 
+                    Health += random.Next(1, 16);
+                    Power += random.Next(3, 6);
+                    Luck += random.Next(1, 4);
+                    Mana += random.Next(10, 21);
+                    break;
+                case CombatClass.Warrior:
+                    Health += random.Next(10, 21);
+                    Power += random.Next(1, 4);
+                    Luck += random.Next(1, 4);
+                    Mana += random.Next(1, 6);
+                    break;
+                case CombatClass.Rogue:
+                    Health += random.Next(1, 16);
+                    Power += random.Next(1, 4);
+                    Luck += random.Next(3, 6);
+                    Mana += random.Next(5, 11);
+                    break;
+            }
+        }
+        public void CalculateTotals()
+        {
+            TotalHealth += Health;
+            TotalPower += Power;
+            TotalLuck += Luck;
+            
         }
     }
 }
